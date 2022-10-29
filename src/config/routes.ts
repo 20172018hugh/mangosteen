23fee4b137b69097f528920a889e6c38dd31c26c
20172018hugh <1772796367@qq.com>
2022-10-29 16:29:58 +0800

    welcome路由配置

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,6 +1,21 @@
+import { Fourth } from './../components/welcome/Fourth';
+import { Third } from './../components/welcome/Third';
+import { Second } from './../components/welcome/Second';
+import { First } from './../components/welcome/First';
 import { Bar } from "../views/Bar";
 import { Foo } from "../views/Foo";
+import { Welcome } from "../views/Welcome";
 export const routes = [
     { path: '/', component: Foo },
     { path: '/bar', component: Bar },
+    {
+        path: '/welcome',
+        component: Welcome,
+        children: [
+            { path: '1', component: First },
+            { path: '2', component: Second },
+            { path: '3', component: Third },
+            { path: '4', component: Fourth },
+        ]
+    },
 ]
