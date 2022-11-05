5d9f0cfb37ef456cad049e7cdbf6f1a86b4ac818
20172018hugh <1772796367@qq.com>
2022-11-05 14:35:01 +0800

    实现滑动切换

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -17,10 +17,10 @@ export const routes = [
         component: Welcome,
         children: [
             { path: '', redirect: '/welcome/1', component: First },
-            { path: '1', components: { main: First, footer: FirstActions }, },
-            { path: '2', components: { main: Second, footer: SecondActions }, },
-            { path: '3', components: { main: Third, footer: ThirdActions }, },
-            { path: '4', components: { main: Fourth, footer: FourthActions }, },
+            { path: '1', name: "Welcome1", components: { main: First, footer: FirstActions }, },
+            { path: '2', name: "Welcome2", components: { main: Second, footer: SecondActions }, },
+            { path: '3', name: "Welcome3", components: { main: Third, footer: ThirdActions }, },
+            { path: '4', name: "Welcome4", components: { main: Fourth, footer: FourthActions }, },
         ]
     },
 ]
