c33af80c1272e787aa0323a604be5d980bcf673c
20172018hugh <1772796367@qq.com>
2022-11-05 23:41:49 +0800

    修复iconName允许为空bug

--- src/shared/FloatButton.tsx
+++ src/shared/FloatButton.tsx
@@ -1,7 +1,13 @@
-import { defineComponent } from 'vue';
-import { Icon } from './Icon';
+import { defineComponent,PropType } from 'vue';
+import { Icon,IconName } from './Icon';
 import s from './FloatButton.module.scss';
 export const FloatButton = defineComponent({
+    props:{
+        iconName:{
+            type:String as PropType<IconName>,
+            required:true,
+        }
+    },
     setup: (props, context) => {
         return () => (
             <div class={s.floatButton}>
